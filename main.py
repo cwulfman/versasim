4cd367be1320498e355219c845b1bbe8b32b5d50
Cliff Wulfman <cwulfman@princeton.edu>
2022-06-02 18:49:30 -0400

    more filling out

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ async def root():
 @app.get("/elections")
 async def get_elections():
     elections = elections_table.all()
-    
+
     return {"elections": elections}
 
 @app.get("/elections/{election_id}")
@@ -36,3 +36,27 @@ async def get_election(election_id):
 async def get_election_report(election_id):
     report = edf.ElectionReport(base) # ignore id for now and return all
     return report.generate_report()
+
+@app.get("/gpunits")
+async def get_gpunits():
+    return base.get_table('GpUnit').all()
+
+@app.get("/offices")
+async def get_offices():
+    return base.get_table('Office').all()
+
+@app.get("/persons")
+async def get_persons():
+    pass
+
+@app.get("/candidates")
+async def get_candidates():
+    pass
+
+@app.get("/contests")
+async def get_contests():
+    pass
+
+@app.get("/ballotstyles")
+async def get_ballotstyles():
+    pass
